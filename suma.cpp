04049e767d2f796329d86a1cc3ea71bc43e0d834
Arroyo Mercedes <mecedes.arroyo@utelvt.edu.ec>
2022-09-09 14:46:06 -0500

    Segundo Versión

--- suma.cpp
+++ suma.cpp
@@ -1,11 +1,13 @@
- #include<iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-	float A,B,C;
-	cin>>A>>B;
-	C=A+B;
-	cout<<C;
-	return 0;
-}
+float A, B, C;
 
+cout<<"Ingrese 2 numero A B:";
+cin>>A>>B;
+C=A+B;
+cout<<"El resultado es :";
+cout<<C;
+return 0;
+}
